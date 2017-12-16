19a5e71b237d6ce1d725cfae6acbc763c6c69eb6
Tim Martin <tim@asymptotic.co.uk>
2017-12-16 08:27:19 +0000

    Changed default font to DejaVu Sans

--- init.el
+++ init.el
@@ -36,7 +36,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Ubuntu Mono" :foundry "unknown" :slant normal :weight normal :height 113 :width normal)))))
+ '(default ((t (:inherit nil :stipple nil :background "white" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 105 :width normal :family "DejaVu Sans Mono")))))
 
 ; Unset some bindings that I never use
 (global-unset-key (kbd "<f1>"))
